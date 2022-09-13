c408311577ff34ac84c03d6a951053f660cf7e05
Roberto González Michelena <robemadz@gmail.com>
2022-09-13 17:31:08 +0200

    backend ready with register, login, and middleware auth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 4000;
 
+//Connect to database
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from json' });
@@ -10,5 +19,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
